bf26fea988c80951dd8f6fdf2917afd1bc4046d1
Eric Nicolas and Verna Santos <verna@>
2018-03-20 10:47:18 -0700

    Initial set-up

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+    $("div#walrus-showing").removeAttr('id');
+    $("div#walrus-showing").addClass("walrus-hello");
+
+  });
+});
